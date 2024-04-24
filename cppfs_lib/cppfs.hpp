bc2e0abb4d0c2f73f613ccf27d7355987c0547a7
ghsi011 <ghsi011@gmail.com>
2024-04-24 20:28:35 +0300

    start poc

--- cppfs_lib/cppfs.hpp
+++ cppfs_lib/cppfs.hpp
@@ -1,5 +1,40 @@
 #pragma once
 
+#include <fstream>
+#include <print>
+
+namespace cppfs 
+{
+
+// todo:
+// 1. read raw file system : C:\
+// 2. parse mft entries
+// 3. do dirlist
+// 4. refactor
+
+void work() 
+{
+    std::fstream c_drive;
+    c_drive.open(R"(c:\)");
+
+    std::string data;
+    data.resize(1000);
+    c_drive.read(data.data(), data.size());
+    int count = 0;
+    for (char d : data) {
+        if (count == 16) {
+            std::println("");
+        }
+        count++;
+        std::print("0x{:x} ", d);
+    }
+
+    c_drive.close();
+}
+
 int test() {
     return 42;
 }
+
+}
+
--- cppfs_test/test.cpp
+++ cppfs_test/test.cpp
@@ -2,7 +2,12 @@
 
 #include "cppfs_lib/cppfs.hpp"
 
+using namespace cppfs;
+
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(test(), 42);
+
+  work();
+
   EXPECT_TRUE(true);
 }
